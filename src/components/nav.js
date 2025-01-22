dd2e1fcd63ffcd60c9f91660fc19478f0d583efd
PHenrique-dev <pedrinho03s4mini@gmail.com>
2025-01-22 14:51:32 -0300

    Navbar quase pronta e primeiro bug

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './nav.css'
-import { MdLocalShipping } from "react-icons/md";function Nav() {
+import { MdLocalShipping } from "react-icons/md";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { FiLogIn } from "react-icons/fi";
+const Nav= () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <>
        <div className='header'>
@@ -10,6 +15,26 @@ import { MdLocalShipping } from "react-icons/md";function Nav() {
             </div>
             <div className='info'>
                 <p>Frete grátis em compras a partir de R$299,90</p>
+            </div>
+            <div className='mid-header'>
+              <div className='logo'>
+                <img src={`${process.env.PUBLIC_URL}/image/435762163_2822109891280408_7450111231118052636_n-removebg-preview.png`}  alt='logo'></img>
+              </div>
+              <div className='search_box'>
+              <input
+              type="text"
+              value=''
+              placeholder="O que você deseja?"></input>
+              <button><BiSearchAlt2/></button>
+              </div>
+              <div className='user'>
+                <div className='icon'>
+                <FiLogIn/>
+                </div>
+                <div className='btn'>
+                  <button onClick={() => loginWithRedirect()}>Login</button>
+                </div>
+              </div>
             </div>
         </div>
         </div> 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-phiga.us.auth0.com"
+    clientId="jiTULAN68BKm449mEYhAOt5Abb9HRVAy"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
